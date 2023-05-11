4286b6a2f3395e77c6c81fbbf360e926a55da289
yunnus <122170438+yunnus143@users.noreply.github.com>
2023-05-11 09:30:30 +0530

    Update RefreshToken.js

--- src/models/RefreshToken.js
+++ src/models/RefreshToken.js
@@ -1,7 +1,10 @@
 const mongooose = require('mongoose');
 
 const tokenSchema = new mongooose.Schema({
-    // Your code goes here
+    token:{
+        type: String,
+        require: true
+    }
 })
 
 const RefreshToken = mongooose.model('tokens', tokenSchema);
