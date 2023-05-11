bfa3787ff802f92279159a9802eec1694c6e2f51
yunnus <122170438+yunnus143@users.noreply.github.com>
2023-05-11 09:47:02 +0530

    Update validation.js

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -3,11 +3,22 @@ const Joi = require("joi");
 
 // Register validation
 const registerValidation = (data) => {
-  // Your code goes here
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  // Lets validate the user's data
+  return schema.validate(data);
 };
 // Login validation
 const loginValidation = (data) => {
-  // Your code goes here
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  // Lets validate the user's data
+  return schema.validate(data);
 };
 
 module.exports.registerValidation = registerValidation;
