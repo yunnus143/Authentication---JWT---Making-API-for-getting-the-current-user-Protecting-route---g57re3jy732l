47268845cabc694f7e53b80a17b7da512ee1080c
yunnus <122170438+yunnus143@users.noreply.github.com>
2023-05-11 09:31:20 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,28 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-  // Your code goes here
+  name:{
+      type: String,
+      required: true,
+      max: 255,
+      min: 6
+  },
+  email:{
+      type: String,
+      required:true,
+      max: 255,
+      min: 6,
+  },
+  password:{
+      type: String,
+      required: true,
+      max: 1024,
+      min: 6,
+  },
+  date:{
+      type: Date,
+      default: Date.now,
+  }
 })
 
 const User = mongoose.model('user',userSchema);
